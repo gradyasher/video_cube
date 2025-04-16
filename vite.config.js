96bfc3efee269d7d86a5111b920e47660223c9d0
gradyasher <gradyasher@gmail.com>
2025-04-15 20:18:19 -0500

    Catalog variant data fix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import glsl from 'vite-plugin-glsl';
 
 export default defineConfig({
   plugins: [react(), glsl()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000', // if running your own local server, not needed for Vercel
+    },
+  },
 });
