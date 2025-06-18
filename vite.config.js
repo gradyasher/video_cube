23c522ccb5675abe1a047d1fd625dce4980427e4
gradyasher <gradyasher@gmail.com>
2025-06-17 23:01:36 -0500

    vercel config? i think this is gitignored anyway

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import glsl from 'vite-plugin-glsl';
 
 export default defineConfig({
+  base: './', // ← This fixes asset paths in production!
   plugins: [react(), glsl()],
   server: {
     proxy: {
