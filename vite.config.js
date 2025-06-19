77843534d39aadb06d36e46ce9d5ea4784f7a354
gradyasher <gradyasher@gmail.com>
2025-06-18 22:57:42 -0500

    okay THIS time it works dont touch it

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,20 @@
 import { defineConfig } from 'vite';
-import { visualizer } from 'rollup-plugin-visualizer';
 import react from '@vitejs/plugin-react';
 import glsl from 'vite-plugin-glsl';
 
 export default defineConfig({
   base: './',
-  plugins: [
-    visualizer({
-      filename: './dist/stats.html',
-      open: true,
-      gzipSize: true,
-      brotliSize: true,
-    }),
-    react(),
-    glsl(),
-  ],
+  plugins: [react(), glsl()],
   server: {
     proxy: {
       '/api': 'http://localhost:3001',
     },
-  },
-  optimizeDeps: {
-    include: ["react", "react-dom"],
-    exclude: ["three-stdlib"]
   },
   build: {
     rollupOptions: {
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            if (id.includes('three')) return 'three';
-            if (id.includes('@react-three')) return 'r3f';
-            if (id.includes('framer-motion')) return 'motion';
-            if (id.includes('troika-three-text')) return 'troika'; // optional
             return 'vendor';
           }
         },
